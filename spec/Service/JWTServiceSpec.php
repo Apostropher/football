937b4a6b2c24a9922c689cd8fef34671a1789e9b
Olatunji Gbadamosi <olatunji@surelygroup.com>
2018-09-16 19:18:01 +0100

    finish speccing service

--- spec/Service/JWTServiceSpec.php
+++ spec/Service/JWTServiceSpec.php
@@ -18,7 +18,7 @@ class JWTServiceSpec extends ObjectBehavior
         $this->beConstructedWith(
             $userRepository,
             $serializer,
-            'test'
+            'dev'
         );
     }
 
@@ -55,12 +55,43 @@ class JWTServiceSpec extends ObjectBehavior
     function it_should_throw_an_exception_if_signature_is_incorrect_during_token_validation() {
         $token = "ewogICAgImFsZyI6ICJIUzI1NiIsCiAgICAidHlwIjogIkpXVCIKfQ==.ewogICAgImlhdCI6IDE1MzcxMTA0MDYsCiAgICAiZXhwIjogMTUzNzExNDAwNiwKICAgICJ0dGwiOiAzNjAwLAogICAgIm5hbWUiOiAiQS1OQU1FIgp9.876c5c81b0a21bc8db57dce06bdb914f44805e1bfa98eb1474a996407efb9e79";
 
-        $this->shouldThrow(FootballException::class)->during('validateToken', [$token]);
+        $this->shouldThrow(new FootballException(JWTService::INVALID_TOKEN_MSG))->during('validateToken', [$token]);
     }
 
-    function it_should_throw_an_exception_if_token_has_expired_during_token_validation() {
+    function it_should_throw_an_exception_if_token_has_expired_during_token_validation(SerializerInterface $serializer) {
+        $payload = new JWTModel\Body();
+
+        $payload->name = 'A-NAME';
+        $payload->ttl = 1;
+        $payload->iat = time() - 3600;
+
         $token = "ewogICAgImFsZyI6ICJIUzI1NiIsCiAgICAidHlwIjogIkpXVCIKfQ==.ewogICAgImlhdCI6IDE1MzcxMTIyMDYsCiAgICAiZXhwIjogMTUzNzExMjIwNywKICAgICJ0dGwiOiAxLAogICAgIm5hbWUiOiAiQS1OQU1FIgp9.dd7609ae6f29e9900cb59bb1347aef642b4eea0d8ecbdcc3529a894559b2e8c2";
 
-        $this->shouldThrow(FootballException::class)->during('validateToken', [$token]);
+        $serializer->deserialize(
+            base64_decode('ewogICAgImlhdCI6IDE1MzcxMTIyMDYsCiAgICAiZXhwIjogMTUzNzExMjIwNywKICAgICJ0dGwiOiAxLAogICAgIm5hbWUiOiAiQS1OQU1FIgp9'),
+            JWTModel\Body::class,
+            'json'
+        )->shouldBeCalled()->willReturn($payload);
+
+        $this->shouldThrow(new FootballException(JWTService::EXPIRED_TOKEN_MSG))->during('validateToken', [$token]);
+    }
+
+    function it_successfully_validates_a_token(SerializerInterface $serializer) {
+        $payload = new JWTModel\Body();
+
+        $payload->name = 'A-NAME';
+        $payload->ttl = 3600;
+        $payload->iat = time();
+        $payload->exp = ($payload->iat + $payload->ttl);
+
+        $token = "ewogICAgImFsZyI6ICJIUzI1NiIsCiAgICAidHlwIjogIkpXVCIKfQ==.ewogICAgImlhdCI6IDE1MzcxMTIyMDYsCiAgICAiZXhwIjogMTUzNzExMjIwNywKICAgICJ0dGwiOiAxLAogICAgIm5hbWUiOiAiQS1OQU1FIgp9.dd7609ae6f29e9900cb59bb1347aef642b4eea0d8ecbdcc3529a894559b2e8c2";
+
+        $serializer->deserialize(
+            base64_decode('ewogICAgImlhdCI6IDE1MzcxMTIyMDYsCiAgICAiZXhwIjogMTUzNzExMjIwNywKICAgICJ0dGwiOiAxLAogICAgIm5hbWUiOiAiQS1OQU1FIgp9'),
+            JWTModel\Body::class,
+            'json'
+        )->shouldBeCalled()->willReturn($payload);
+        
+        $this->validateToken($token)->shouldBeEqualTo($payload->name);
     }
 }
